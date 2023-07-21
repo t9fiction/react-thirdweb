e23fe83472839fd340a38c2644517bc5a00a3660
t9fiction <sohail.sohailishaq@gmail.com>
2023-07-21 15:20:18 +0500

    chainid added

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,23 +1,30 @@
-import React from 'react'
-import ExpirationTime from './ExpirationTime'
-import BalanceOf from './BalanceOf'
+import React from "react";
+import ExpirationTime from "./ExpirationTime";
+import BalanceOf from "./BalanceOf";
 import { useAddress } from "@thirdweb-dev/react";
+import { useChainId } from "@thirdweb-dev/react";
 
 const MainPage = () => {
-    const address = useAddress();
+  const address = useAddress();
+  const chainId = useChainId();
+  console.log("chainID : ",chainId)
   return (
-    <div className='bg-slate-300 h-screen py-24 px-12'>
-        {
-            address ?
-            <>
-            <BalanceOf />
-            <ExpirationTime />
-            </>:
-            <p>Connect wallet to check your membership</p> 
-
-        }
+    <div className="bg-slate-300 h-screen py-24 px-12">
+      {address ? (
+        chainId === 56 ?        
+        <>
+          <BalanceOf />
+          <ExpirationTime />
+        </> 
+        :
+        <p className="font-semibold text-lg">
+          Pleae select Binance Chain and then try again
+        </p>
+      ) : (
+        <p>Connect wallet to check your membership</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default MainPage
+export default MainPage;
